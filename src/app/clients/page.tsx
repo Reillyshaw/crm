a10a4828b8a0ed9bb8a61c53e6d1d2c9b8e8f4d4
Reilly Shaw <reillyshaw7@gmail.com>
2025-07-05 23:13:42 +0100

    Fix import path for ProjectDetailsModal

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import ProjectDetailsModal from "@/clients/ProjectDetailsModal";
+import ProjectDetailsModal from "./ProjectDetailsModal";
 
 type Project = {
   id: number;
